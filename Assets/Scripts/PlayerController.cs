ef7a4a9442f94d7ac036e5fc2350309823bdc1af
Alper Gungormusler <alpynaq@gmail.com>
2014-02-07 21:53:45 +0000

    Simple movement is implemented.
    Basic movement for the player is added.
    Collision detection for walls is implemented.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Transform player;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameEventManager.CurrentState == GameEventManager.GameState.Running)
+        {
+            // Get Input
+            int dx = Input.GetButtonDown("Horizontal") ? (int)Input.GetAxisRaw("Horizontal") : 0;
+            int dy = Input.GetButtonDown("Vertical") ? (int)Input.GetAxisRaw("Vertical") : 0;
+
+            // If input is valid
+            if (dx != 0 || dy != 0)
+            {
+                // Get the next direction
+                Vector3 direction = new Vector3(dx, dy, 0);
+
+                // Check collisions
+                RaycastHit2D hit = Physics2D.Raycast(player.position + direction, direction, 0.0f);
+                if (hit != null && hit.collider != null)
+                {
+                    // Collision detected
+                    if (hit.collider.tag == "Wall")
+                    {
+                        Debug.Log("Collided with " + hit.collider.tag);
+                        return;
+                    }
+                }
+
+                // Translate if not collided
+                player.position += direction;
+            }
+        }
+    }
+}

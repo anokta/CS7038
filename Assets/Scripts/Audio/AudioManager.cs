d4045aa5249d3e39732e1424cca3a6101ea0d28d
Alper Gungormusler <alpynaq@gmail.com>
2014-02-13 17:35:22 +0000

    Audio manager is added (for later use)

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public AudioSource collectSfx, pushSfx;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlaySFX(string type)
+    {
+        switch (type)
+        {
+            case "Push":
+                pushSfx.Play();
+                break;
+
+            case "Collect":
+                collectSfx.Play();
+                break;
+        }
+    }
+}

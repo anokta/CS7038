820b5c92571a89e0782bd901eddbb57b0bcdc4b0
Alper Gungormusler <alpynaq@gmail.com>
2014-03-19 02:09:38 +0000

    Patient treatment takes time now
    Will be improved later.
    + Gui progress bar needed (as in fountain)

--- Assets/Scripts/Entities/Switchables/Patient.cs
+++ Assets/Scripts/Entities/Switchables/Patient.cs
@@ -5,12 +5,39 @@ public class Patient : Switchable
 {
     private bool treated;
 	public Sprite treatedSprite;
+
+    private bool isHeld;
+
+    private Timer timer;
+
+    PlayerController player;
+    Vector2 lastPlayerDirection;
+
     // Use this for initialization
     protected override void Start()
     {
         base.Start();
 
         treated = false;
+
+        player = GameObject.FindObjectOfType<PlayerController>();
+
+        timer = new Timer(0.6f, Success);
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        timer.Update();
+
+        if (isHeld)
+        {
+            if (!player.IsHeld || player.NextDirection != lastPlayerDirection)
+            {
+                Interrupted();
+            }
+        }
     }
 
     public bool IsTreated()
@@ -22,14 +49,36 @@ public class Patient : Switchable
     {
         if (!treated)
         {
-            audioManager.PlaySFX("Treated");
+            isHeld = true;
 
-            var controller = FindObjectOfType<HandController>();
-			controller.value = HandController.MinValue;
-			var renderer = GetComponent<SpriteRenderer>();
-			renderer.sprite = treatedSprite;
+            timer.Reset();
+
+            lastPlayerDirection = player.NextDirection;
         }
+    }
+
+    void Success()
+    {
+        spriteRenderer.sprite = treatedSprite;
+
+        Interrupted();
+
+        audioManager.PlaySFX("Treated");
+
+        player.GetComponent<HandController>().value = HandController.MinValue;
 
         treated = true;
+    }
+
+    void Interrupted()
+    {
+        isHeld = false;
+
+        timer.Stop();
+
+        if (player.AnimState == PlayerController.PlayerAnimState.Wash)
+        {
+            player.AnimState = PlayerController.PlayerAnimState.Idle;
+        }
     }
 }

187db98cf6327e558b86631f4dc6131b00ae1a7c
Alper Gungormusler <alpynaq@gmail.com>
2014-05-28 02:52:05 +0100

    Twitter integration is implemented
    + for both Android & iOS

--- Assets/Scripts/GUI/LevelOverGUI.cs
+++ Assets/Scripts/GUI/LevelOverGUI.cs
@@ -9,6 +9,7 @@ using LOR = GameWorld.LevelOverReason;
 public class LevelOverGUI : MonoBehaviour
 {
     private FacebookIntegration facebook;
+	private TwitterIntegration twitter;
     
     private float _actualWindowSize;
     private float _actualButtonSize;
@@ -213,6 +214,7 @@ public class LevelOverGUI : MonoBehaviour
     void Start()
     {
         facebook = FindObjectOfType<FacebookIntegration>();
+		twitter = FindObjectOfType<TwitterIntegration> ();
 
         GroupManager.main.group["Level Over"].Add(this);
         GroupManager.main.group["Level Over"].Add(this, new GroupDelegator(null, Enter, null));
@@ -473,8 +475,17 @@ public class LevelOverGUI : MonoBehaviour
     }
 
     void ShareToTwitter(string textToDisplay, string urlToDisplay)
-    {
-        Application.OpenURL("http://twitter.com/intent/tweet?text=" + WWW.EscapeURL(textToDisplay) + "&amp;url=" + WWW.EscapeURL(urlToDisplay) + "&amp;lang=en");
+	{
+		#if UNITY_IPHONE || UNITY_ANDROID
+			twitter.Post(textToDisplay, urlToDisplay);
+		#else 
+			string url = "http://twitter.com/intent/tweet?text=" + WWW.EscapeURL(textToDisplay) + "&amp;url=" + WWW.EscapeURL(urlToDisplay) + "&amp;lang=en";
+			string name = "Share on Twitter";
+			
+			Application.ExternalEval("window.open('" + url + "','" + name + "')");
+
+			//Application.OpenURL("http://twitter.com/intent/tweet?text=" + WWW.EscapeURL(textToDisplay) + "&amp;url=" + WWW.EscapeURL(urlToDisplay) + "&amp;lang=en");
+		#endif
     }
 
     void ShareToFacebook(string textToDisplay, string urlToDisplay)
@@ -482,7 +493,12 @@ public class LevelOverGUI : MonoBehaviour
 		#if UNITY_IPHONE || UNITY_ANDROID
 			facebook.Post (textToDisplay, urlToDisplay);	   		
 		#else
-			Application.OpenURL("http://www.facebook.com/sharer/sharer.php?u=" + WWW.EscapeURL(urlToDisplay) + "&t=" + WWW.EscapeURL(textToDisplay));  
+			string urlToDisplay = ""http://www.facebook.com/sharer/sharer.php?u=" + WWW.EscapeURL(urlToDisplay) + "&t=" + WWW.EscapeURL(textToDisplay);
+			string name = "Share on Facebook";
+			
+			Application.ExternalEval("window.open('" + url + "','" + name + "')");
+
+			//Application.OpenURL("http://www.facebook.com/sharer/sharer.php?u=" + WWW.EscapeURL(urlToDisplay) + "&t=" + WWW.EscapeURL(textToDisplay));  
 		#endif    
 	}
 }
---
+++ Assets/Scripts/Social Integration/TwitterIntegration.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Grouping;
+using Prime31;
+
+public class TwitterIntegration : MonoBehaviour {
+
+	#if UNITY_IPHONE || UNITY_ANDROID
+
+	private string status, url;
+
+	private bool popupActive = false;
+	private string popupMessage = "";
+	private float popupY;
+
+	void Start () {
+		TwitterCombo.init( "k6lpYZrWpvVVICQQHVQVkGCIT", "rCEaAZmgFEemPJ7s18cVkgRLQ5JegoR9vO97uya8F1vBl7rgaY" );
+
+		GroupManager.main.group["Level Over"].Add(this);
+
+	}
+
+	void OnEnable()
+	{
+		TwitterManager.loginSucceededEvent += LoginSucceeded;
+		TwitterManager.loginFailedEvent += LoginFailed;
+		TwitterManager.requestDidFinishEvent += PostTweetSucceeded;
+		TwitterManager.requestDidFailEvent += PostTweetFailed;
+		
+		popupY = 1.0f;
+	}
+
+	void OnDisable()
+	{
+		TwitterManager.loginSucceededEvent -= LoginSucceeded;
+		TwitterManager.loginFailedEvent -= LoginFailed;
+		TwitterManager.requestDidFinishEvent -= PostTweetSucceeded;
+		TwitterManager.requestDidFailEvent -= PostTweetFailed;
+	}
+
+	void LoginSucceeded(object result)
+	{
+		TwitterCombo.postStatusUpdate (status + " " + url);
+	}
+
+	void LoginFailed(string error)
+	{
+		popupMessage = "Login to Twitter failed.";
+		popupActive = true;
+	}
+
+	void PostTweetSucceeded(object result)
+	{
+		if (TwitterCombo.isLoggedIn ()) {
+			popupMessage = "Tweet is posted!";
+			popupActive = true;
+		}
+	}
+
+	void PostTweetFailed(string error)
+	{		
+		popupMessage = "Tweet could not be posted.";
+		popupActive = true;
+	}
+
+	void OnGUI()
+	{
+		if (popupActive || popupY < 1.0f) {
+			if (Input.GetMouseButtonDown (0)) {
+				popupActive = false;		
+			}
+
+			popupY = Mathf.Lerp (popupY, popupActive ? 0.9f : 1.1f, Time.deltaTime * 4);
+
+			GUI.Window(5, new Rect(Screen.width * 0.5f - Screen.height * 0.15f, Screen.height * popupY, Screen.height * 0.3f, Screen.height * 0.1f), DoPopup, "", GUIManager.Style.inGameWindow);
+		}
+	}
+
+	void DoPopup(int id)
+	{
+		GUILayout.FlexibleSpace ();
+		GUILayout.BeginHorizontal ();
+		GUILayout.FlexibleSpace ();
+		GUILayout.Label (popupMessage, GUIManager.skin.GetStyle("log"));
+		GUILayout.FlexibleSpace ();
+		GUILayout.EndHorizontal ();
+		GUILayout.FlexibleSpace ();
+	}
+
+	public void Post(string status, string url)
+	{
+		this.status = status;
+		this.url = url;
+
+		TwitterCombo.showLoginDialog();		
+	}
+
+	#endif
+
+}
